66fc76afb782b1ab8ecea7e13bf159160e2d0d65
Kevin Moore <kevin@thinkpixellab.com>
2010-07-30 05:14:25 -0500

    Build now supports compiling sources

--- build.rb
+++ build.rb
@@ -4,28 +4,53 @@ unless defined?(PROJECT_ROOT)
    PROJECT_ROOT = File.dirname(__FILE__)
 end
 
-def calc_deps
+def calc_deps(compile = false)
   js_path = File.join(PROJECT_ROOT, 'js')
-  closure_path = File.join('lib', 'closure-library','closure')
+  closure_path = File.join('lib', 'closure-library', 'closure')
 
-  calcdeps_path = File.join(closure_path,'bin','calcdeps.py')
+  calcdeps_path = File.join(closure_path, 'bin', 'calcdeps.py')
 
   sys_command = "python #{calcdeps_path}"
 
-  sys_command << " -d #{closure_path} -o deps"
+  js_dirs = ['box2d', 'demo']
 
-  app_path = File.join(js_path, 'application.js')
-  sys_command << " -i #{app_path}"
+  if(compile)
+    # only relavant for list and script output
+    app_path = File.join(js_path, 'application.js')
+    sys_command << " -i #{app_path}"
 
-  output_path = File.join(js_path, 'deps.js')
+    output_path = File.join(js_path, 'compiled.js');
+    sys_command << " --output_mode compiled"
 
-  ['box2d','demo'].each do |files_dir|
-    sys_command << " -p #{File.join(js_path, files_dir)}"
+    jar_path = File.join(PROJECT_ROOT, 'vendor', 'closure_compiler', 'compiler.jar')
+    sys_command << " --compiler_jar #{jar_path}"
+
+    files = []
+    js_dirs.each do |files_dir|
+      files_dir = File.join(js_path, files_dir)
+      files.concat(Dir["#{files_dir}/**/*.js"])
+    end
+    
+    files.each do |js_file|
+      sys_command << " --dep #{js_file}"
+    end
+    
+    sys_command << " --compiler_flag --manage_closure_dependencies=true"
+
+  else
+    js_dirs.each do |files_dir|
+      sys_command << " --path #{File.join(js_path, files_dir)}"
+    end
+
+    sys_command << " --dep #{closure_path}"
+
+    output_path = File.join(js_path, 'deps.js')
+
+    sys_command << " --output_mode deps"
+
   end
 
   sys_command << " --output_file=#{output_path}"
-
-  #python path-to-closure-library/closure/bin/calcdeps.py -i path-to-your-src/requirements.js -o deps -d path-to-closure-library/closure/ -p path-to-your-src/ --output_file=path-to-your-src/deps.js
 
   puts sys_command
   `#{sys_command}`
