67d5ab3fb8ca8f708d80203794aded18fda097ad
Kevin Moore <kevin@thinkpixellab.com>
2010-07-30 19:01:43 -0500

    Removing prototype usage in application.js

--- js/application.js
+++ js/application.js
@@ -3,5 +3,5 @@ goog.require('demo');
 var _demo;
 
 Event.observe(window, 'load', function() {
-  _demo = new demo($('canvas'));
+  _demo = new demo(document.getElementsByTagName('canvas')[0]);
 });
