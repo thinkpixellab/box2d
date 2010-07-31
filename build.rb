11664dca0bf6f96e543a95fe1b1ed8627c57e64d
Kevin Moore <kevin@thinkpixellab.com>
2010-07-30 19:44:27 -0500

    Compile is now direct to the jar file...

--- build.rb
+++ build.rb
@@ -7,61 +7,63 @@ end
 JS_PATH = File.join(PROJECT_ROOT, 'js')
 CLOSURE_PATH = File.join('lib', 'closure-library', 'closure')
 JAR_PATH = File.join(PROJECT_ROOT, 'vendor', 'closure_compiler', 'compiler.jar')
-JS_DIRS = ['box2d', 'demo']
+JS_FILE_DIRS = ['box2d', 'demo']
 
-def calc_deps(compile = false)
+def make_deps
 
   calcdeps_path = File.join(CLOSURE_PATH, 'bin', 'calcdeps.py')
 
   sys_command = "python #{calcdeps_path}"
 
-
-  if(compile)
-    # only relavant for list and script output
-    app_path = File.join(JS_PATH, 'application.js')
-    sys_command << " -i #{app_path}"
-
-    output_path = File.join(JS_PATH, 'compiled.js');
-    sys_command << " --output_mode compiled"
-
-    sys_command << " --compiler_jar #{JAR_PATH}"
-
-    files = []
-    JS_DIRS.each do |files_dir|
-      files_dir = File.join(JS_PATH, files_dir)
-      files.concat(Dir["#{files_dir}/**/*.js"])
-    end
-
-    files.each do |js_file|
-      sys_command << " --dep #{js_file}"
-    end
-
-    {
-      'compilation_level' => 'ADVANCED_OPTIMIZATIONS',
-      'manage_closure_dependencies' => 'true',
-      'summary_detail_level' => '3',
-      'debug' => 'true',
-      'warning_level' => 'VERBOSE'
-    }.each do |key, value|
-      sys_command << " --compiler_flag --#{key}=#{value}"
-    end
-  else
-    JS_DIRS.each do |files_dir|
-      sys_command << " --path #{File.join(JS_PATH, files_dir)}"
-    end
-
-    sys_command << " --dep #{CLOSURE_PATH}"
-
-    output_path = File.join(JS_PATH, 'deps.js')
-
-    sys_command << " --output_mode deps"
-
+  JS_FILE_DIRS.each do |files_dir|
+    sys_command << " --path #{File.join(JS_PATH, files_dir)}"
   end
 
+  sys_command << " --dep #{CLOSURE_PATH}"
+
+  output_path = File.join(JS_PATH, 'deps.js')
+
+  sys_command << " --output_mode deps"
   sys_command << " --output_file=#{output_path}"
 
   puts sys_command
   `#{sys_command}`
 end
 
-calc_deps
+def init_compile
+  output_path = File.join(JS_PATH, 'compiled.js')
+
+  files = []
+  files << 'js/application.js'
+  JS_FILE_DIRS.each do |js_dir|
+    js_dir = File.join(JS_PATH, js_dir)
+    files.concat(Dir["#{js_dir}/**/*.js"])
+  end
+
+  files << "lib/closure-library/closure/goog/base.js"
+
+  compile(output_path, files)
+end
+
+def compile(output_file, js_file_paths)
+  sys_command = "java -jar #{JAR_PATH}"
+
+  js_file_paths.each do |file|
+    sys_command << " --js #{file}"
+  end
+
+  sys_command << " --js_output_file #{output_file} --compilation_level ADVANCED_OPTIMIZATIONS --summary_detail_level 3 --debug true --warning_level VERBOSE --manage_closure_dependencies true"
+  #sys_command << " --js_output_file #{output_file}"
+
+  # look at tree
+  # sys_command << "  --js_output_file #{compiled_output_path}.tree --compilation_level ADVANCED_OPTIMIZATIONS --print_tree true"
+
+  # easy way to see help
+  # sys_command = "java -jar #{JAR_PATH} --help"
+
+  puts sys_command.inspect
+  `#{sys_command}`
+end
+
+
+make_deps
