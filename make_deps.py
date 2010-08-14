f16997d4c53dc9b31b0781b1d645ae3094997ff7
Kevin Moore <kevin@thinkpixellab.com>
2010-08-14 16:18:09 -0300

    Removed un-needed var definition

--- make_deps.py
+++ make_deps.py
@@ -7,13 +7,12 @@ js_path = "js"
 js_dirs = map(lambda dir: os.path.join(js_path, dir), ['box2d','demo'])
 application_js_path = os.path.join(js_path, 'application.js')
 closure_path = os.path.join('lib', 'closure-library','closure')
-calcdeps_py_path = os.path.join(closure_path, "bin", "calcdeps.py")
 
 closure = Closure(
   closure_path = closure_path,
   application_js_path = application_js_path,
   closure_dependencies = js_dirs + [application_js_path],
-  calcdeps_py_path = calcdeps_py_path,
+  calcdeps_py_path = os.path.join(closure_path, "bin", "calcdeps.py"),
   deps_js_path = os.path.join(js_path, "deps.js"),
   compiled_js_path = os.path.join(js_path, "compiled.js"),
   extern_dir = os.path.join(js_path, 'externs')
