cc7937968f619b4f2fbd87fd371c3b4755011ebc
Kevin Moore <kevin@thinkpixellab.com>
2010-08-14 18:30:04 -0300

    Tightening up location of closure files...and script

--- build.py
+++ build.py
@@ -6,13 +6,10 @@ from _tools.ClosureShared import Closure
 js_path = "js"
 js_dirs = map(lambda dir: os.path.join(js_path, dir), ['box2d','demo'])
 application_js_path = os.path.join(js_path, 'application.js')
-closure_path = os.path.join('lib', 'closure-library','closure')
 
 closure = Closure(
-  closure_path = closure_path,
   application_js_path = application_js_path,
   closure_dependencies = js_dirs + [application_js_path],
-  calcdeps_py_path = os.path.join(closure_path, "bin", "calcdeps.py"),
   deps_js_path = os.path.join(js_path, "deps.js"),
   compiled_js_path = os.path.join(js_path, "compiled.js"),
   extern_dir = os.path.join(js_path, 'externs')
