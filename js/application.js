eff3946b75833b91a78534030500e726f0dabf5d
Kevin Moore <kevin@thinkpixellab.com>
2010-08-14 21:12:45 -0300

    Adding jQuery for window load event

--- js/application.js
+++ js/application.js
@@ -1,9 +1,7 @@
 goog.require('demo');
 goog.require('goog.events');
 goog.require('goog.events.EventType');
-function load() {
-  goog.events.listen(window, goog.events.EventType.LOAD, function() {
-    new demo(document.getElementsByTagName('canvas')[0]);
-  });
-};
-goog.exportSymbol('load', load);
+
+$(window).load(function() {
+  new demo(document.getElementsByTagName('canvas')[0]);
+});
---
+++ js/externs/jquery.js
@@ -0,0 +1,79 @@
+// copied as needed from
+// http://closure-compiler.googlecode.com/svn/trunk/contrib/externs/jquery-1.3.2.externs.js
+/**
+ * @param {Object|string} arg
+ * @return {jQueryObject}
+ */
+var $ = function(arg) {};
+
+/** @constructor */
+function jQueryObject() {};
+
+/**
+ * @param {Function} fn
+ * @return {jQueryObject}
+ */
+jQueryObject.prototype.resize = function(fn) {};
+
+/**
+ * @param {Function} fn
+ * @return {jQueryObject}
+ */
+jQueryObject.prototype.mousedown = function(fn) {};
+/**
+ * @param {Function} fn
+ * @return {jQueryObject}
+ */
+jQueryObject.prototype.mouseleave = function(fn) {};
+/**
+ * @param {Function} fn
+ * @return {jQueryObject}
+ */
+jQueryObject.prototype.mousemove = function(fn) {};
+/**
+ * @param {Function} fn
+ * @return {jQueryObject}
+ */
+jQueryObject.prototype.mouseup = function(fn) {};
+/**
+ * @param {Function} fn
+ * @return {jQueryObject}
+ */
+jQueryObject.prototype.keypress = function(fn) {};
+
+/**
+ * @return {Object}
+ */
+jQueryObject.prototype.offset = function() {};
+/**
+ * @param {(number|string)=} opt_speed
+ * @param {Function=} opt_fn
+ * @return {jQueryObject}
+ */
+jQueryObject.prototype.fadeIn = function(opt_speed, opt_fn) {};
+
+/**
+ * @param {(number|string)=} opt_speed
+ * @param {Function=} opt_fn
+ * @return {jQueryObject}
+ */
+jQueryObject.prototype.fadeOut = function(opt_speed, opt_fn) {};
+
+/**
+ * @param {(number|string)=} opt_speed
+ * @return {jQueryObject}
+ */
+jQueryObject.prototype.delay = function(opt_speed) {};
+
+/**
+ * @param {(number|string)=} opt_speed
+ * @param {Function=} opt_fn
+ * @return {jQueryObject}
+ */
+jQueryObject.prototype.hide = function(opt_speed, opt_fn) {};
+
+/**
+ * @param {string} opt_val
+ * @return {string|jQueryObject}
+ */
+jQueryObject.prototype.html = function(opt_val) {};
