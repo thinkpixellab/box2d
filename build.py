70f09c844e8f9899b38e31f2ba27f066adcd0e78
Kevin Moore <kevin@thinkpixellab.com>
2010-09-15 10:31:28 -0700

    build should not be debug by default

--- build.py
+++ build.py
@@ -19,6 +19,6 @@ Closure(
   deps_js_path = deps_js_path,
   compiled_js_path = compiled_js_path,
   extern_files = [jquery_extern]
-).build_and_process('index_source.html', 'index_compiled.html', debug = False, skip_build = True)
+).build_and_process('index_source.html', 'index_compiled.html', debug = False, skip_build = False)
 
 HtmlCompressor('index_compiled.html', 'index.html', 'js/compressed.js').compress()
