98aa19fc51b44d5c99b585e3ee5d7990697bee49
Kevin Moore <kevin@thinkpixellab.com>
2010-09-30 11:19:28 -0700

    Updated build and index_source for new locations

--- build.py
+++ build.py
@@ -6,6 +6,9 @@ from _tools import HtmlPost
 from _tools.HtmlCompressor import HtmlCompressor
 
 js_path = "js"
+
+closure_path = os.path.join(js_path, 'closure-library','closure')
+
 js_dirs = map(lambda dir: os.path.join(js_path, dir), ['box2d','demo','pixelLab'])
 application_js_path = os.path.join(js_path, 'application.js')
 deps_js_path = os.path.join(js_path, "deps.js")
@@ -14,6 +17,7 @@ compiled_js_path = os.path.join(js_path, "compiled.js")
 jquery_extern = os.path.join(js_path, 'externs', 'jquery.js')
 
 Closure(
+  closure_path = closure_path,
   application_js_path = application_js_path,
   closure_dependencies = js_dirs + [application_js_path],
   deps_js_path = deps_js_path,
